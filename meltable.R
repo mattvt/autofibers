9969eaf5a2931a25e784cc3091dddd83f606cfc1
mflamini <mflamini@vt.edu>
2019-04-24 11:41:23 -0400

    delete copies of R/ files

--- meltable.R
+++
@@ -1,9 +0,0 @@
-
-#return a meltable dataframe without any of the time dependent studd
-#' @export
-meltable <- function(df){
-
-minusSS <- unique(subset(df, select = -c(time, forces,stroke,ym, strain, stressSEM, stressFTIR, stressHyb, dstrain, dstress )))
-minusSS <- plyr::melt(minusSS)
-return(minusSS)
-}
