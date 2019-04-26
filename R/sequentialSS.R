906e6d2d4a534bda3fc2d6c3e5b6ce434d48e46f
mflamini <mflamini@vt.edu>
2019-04-26 13:18:37 -0400

    added/removed some comments

--- R/sequentialSS.R
+++ R/sequentialSS.R
@@ -8,9 +8,12 @@ width <- length(unique(all$temp)) + 3
   library(ggplot2)
   library(gridExtra)
 
+  ################################
+  ##### ym vs strain in a row ###
+  ################################
+
   gg <- ggplot2::ggplot(all, aes(x = strain, y = ym))+
     geom_line(size = 1.5)+
-    #scale_color_brewer(palette = "RdBu", direction=-1)+
     xlab('Strain')+
     ylab('Stress (GPa)')+
     scale_x_continuous(breaks = pretty(all$strain, n = 2))+
@@ -21,25 +24,12 @@ width <- length(unique(all$temp)) + 3
     theme(legend.position='none', panel.spacing.x=unit(1.2, "lines"))+
     facet_grid(. ~ temp, space = "free")
 
-
- #   png(filename=paste(path,"/ymAllanneal500.png", sep = ""),
-#        type="cairo",
-#        units="in",
-#        width=width,
-#        height=2,
-#        pointsize=12,
-#        res=1000)
-
-# print(gg)
-#  dev.off()
   ################################
   ##### Stress Strain in a row ###
   ################################
 
-
   gg2 <- ggplot2::ggplot(all, aes(x = strain, y = stressFTIR))+
     geom_line(size = 1.5)+
-   # scale_color_brewer(palette = "RdBu", direction=-1)+
     xlab('Strain')+
     ylab('Stress (GPa)')+
     scale_x_continuous(breaks = pretty(all$strain, n = 2))+
@@ -49,18 +39,6 @@ width <- length(unique(all$temp)) + 3
     theme_minimal()+
     theme(legend.position='none', panel.spacing.x=unit(1.2, "lines"))+
     facet_grid(. ~ temp, space = "free")
-
-
-#   png(filename=paste(path,"/ssAllanneal500.png", sep = ""),
-#        type="cairo",
-#        units="in",
-#        width=width,
-#        height=2,
-#        pointsize=12,
-#        res=1000)
-
-#  print(gg2)
-#  dev.off()
 
   ###################
   #Combine the two###
