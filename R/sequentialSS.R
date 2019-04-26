282d20aece3ba2ade09b45c7f012e981f337a1f1
mflamini <mflamini@vt.edu>
2019-04-26 14:04:52 -0400

    updated the y axis label of ym graph from stress to dstress

--- R/sequentialSS.R
+++ R/sequentialSS.R
@@ -9,13 +9,13 @@ width <- length(unique(all$temp)) + 3
   library(gridExtra)
 
   ################################
-  ##### ym vs strain in a row ###
-  ################################
+##### ym vs strain in a row ###
+################################
 
-  gg <- ggplot2::ggplot(all, aes(x = strain, y = ym))+
-    geom_line(size = 1.5)+
+gg <- ggplot2::ggplot(all, aes(x = strain, y = ym))+
+  geom_line(size = 1.5)+
     xlab('Strain')+
-    ylab('Stress (GPa)')+
+    ylab('dStress (GPa)')+
     scale_x_continuous(breaks = pretty(all$strain, n = 2))+
     labs(title = "Derivitive of stress with respect to strain",
          subtitle = "",
@@ -24,29 +24,29 @@ width <- length(unique(all$temp)) + 3
     theme(legend.position='none', panel.spacing.x=unit(1.2, "lines"))+
     facet_grid(. ~ temp, space = "free")
 
-  ################################
-  ##### Stress Strain in a row ###
-  ################################
+################################
+##### Stress Strain in a row ###
+################################
 
-  gg2 <- ggplot2::ggplot(all, aes(x = strain, y = stressFTIR))+
-    geom_line(size = 1.5)+
-    xlab('Strain')+
-    ylab('Stress (GPa)')+
-    scale_x_continuous(breaks = pretty(all$strain, n = 2))+
-    labs(title = "Stress vs Strain for each Temp",
-         subtitle = "",
-         color = 'Temperature')+
-    theme_minimal()+
-    theme(legend.position='none', panel.spacing.x=unit(1.2, "lines"))+
-    facet_grid(. ~ temp, space = "free")
+gg2 <- ggplot2::ggplot(all, aes(x = strain, y = stressFTIR))+
+  geom_line(size = 1.5)+
+  xlab('Strain')+
+  ylab('Stress (GPa)')+
+  scale_x_continuous(breaks = pretty(all$strain, n = 2))+
+  labs(title = "Stress vs Strain for each Temp",
+       subtitle = "",
+       color = 'Temperature')+
+  theme_minimal()+
+  theme(legend.position='none', panel.spacing.x=unit(1.2, "lines"))+
+  facet_grid(. ~ temp, space = "free")
 
-  ###################
-  #Combine the two###
-  ###################
+###################
+#Combine the two###
+###################
 
-  png(filename=paste(path,"/ymAndssSequential_", name, ".png", sep = ""),
-      type="cairo",
-      units="in",
+png(filename=paste(path,"/SequentialSS_", name, ".png", sep = ""),
+    type="cairo",
+    units="in",
       width=width,
       height=5,
       pointsize=12,
