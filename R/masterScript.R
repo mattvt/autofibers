e2a5780cf28145359e4474941fdc2cbac25d7acd
mflamini <mflamini@vt.edu>
2019-04-26 14:03:35 -0400

    changed filter width from .1 to .01.

--- R/masterScript.R
+++ R/masterScript.R
@@ -15,47 +15,47 @@ combineData <- function(n = 300, path = getwd(), condition = "condition"){
   library(reshape2)
 
 
-#choose ftirSEM file and mech tests
-ftirSemPath <- file.choose()
-mechtestsPath <- choose.files()
+  #choose ftirSEM file and mech tests
+  ftirSemPath <- file.choose()
+  mechtestsPath <- choose.files()
 
-#read ftirsem
-ftirSem <- read.csv(ftirSemPath)
-colnames(ftirSem)[1] <- 'temp'
+  #read ftirsem
+  ftirSem <- read.csv(ftirSemPath)
+  colnames(ftirSem)[1] <- 'temp'
 
-# and mechtests
-names(mechtestsPath) <- basename(mechtestsPath)
-renamed <- names(mechtestsPath)
-renamed <- substr(renamed, 1, 2)
-names(mechtestsPath) <- renamed
+  # and mechtests
+  names(mechtestsPath) <- basename(mechtestsPath)
+  renamed <- names(mechtestsPath)
+  renamed <- substr(renamed, 1, 2)
+  names(mechtestsPath) <- renamed
 
-allMech <- ldply(mechtestsPath, read.csv)
-allMech <- allMech[1:4]
-names(allMech) <- c('temp','time','forces','stroke')
+  allMech <- ldply(mechtestsPath, read.csv)
+  allMech <- allMech[1:4]
+  names(allMech) <- c('temp','time','forces','stroke')
 
-#Get rid of non numeric headers for each temperature
-allMech <- allMech[!is.na(as.numeric(as.character(allMech$force))),]
+  #Get rid of non numeric headers for each temperature
+  allMech <- allMech[!is.na(as.numeric(as.character(allMech$force))),]
 
-# force everything back to numeric
-allMech$forces <- as.numeric(as.character(allMech$forces))
-allMech$stroke <- as.numeric(as.character(allMech$stroke))
-allMech$time <- as.numeric(as.character(allMech$time))
+  # force everything back to numeric
+  allMech$forces <- as.numeric(as.character(allMech$forces))
+  allMech$stroke <- as.numeric(as.character(allMech$stroke))
+  allMech$time <- as.numeric(as.character(allMech$time))
 
-#take every n number of rows
-n <- n * length(unique(allMech$temp))
-n <- length(allMech$temp) %/% n
-allMech <- allMech[seq(1, nrow(allMech), n), ]
+  #take every n number of rows
+  n <- n * length(unique(allMech$temp))
+  n <- length(allMech$temp) %/% n
+  allMech <- allMech[seq(1, nrow(allMech), n), ]
 
-allMech <- smoothEach(allMech, .1)
-allMech <- na.omit(allMech)
+  allMech <- smoothEach(allMech, .01)
+  allMech <- na.omit(allMech)
 
-# Merge allMech and ftirSem and calculate stress vs strain
+  # Merge allMech and ftirSem and calculate stress vs strain
 
-all <- merge(allMech, ftirSem, by = 'temp')
-all$stressFTIR <- all$forces/all$CSA_FTIR/1000
-all$stressSEM <- all$forces/all$CSA_SEM/1000
-#all$stressHyb <- all$forces/all$Hybrid/1000
-all$strain <- all$stroke/12.5
+  all <- merge(allMech, ftirSem, by = 'temp')
+  all$stressFTIR <- all$forces/all$CSA_FTIR/1000
+  all$stressSEM <- all$forces/all$CSA_SEM/1000
+  #all$stressHyb <- all$forces/all$Hybrid/1000
+  all$strain <- all$stroke/12.5
 
 #get youngs modulus and maximum strength
 all <- getYMall(all)
