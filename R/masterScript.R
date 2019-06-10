1165483ce20e3f853a052df534ab727da2c20528
mflamini <mflamini@vt.edu>
2019-06-10 12:52:23 -0400

    master: integrated fityk data in to the process peak aread: wrote two functions that extract the area data from the fityk data set.

--- R/masterScript.R
+++ R/masterScript.R
@@ -1,22 +1,27 @@
 #'The master function for this package
 #'
 #'This function prompts the user to first select a .csv file containing the
-#'dataframe which contains the data collected for each temperature. It then prompts
-#'the user to select all of the mechanical tests. It then processes and reshapes
+#'dataframe which contains the ftir and SEM data collected for each temperature. It then prompts
+#'the user to select all of the mechanical test csv's. It then processes and reshapes
 #'the data so that it can be easilly plotted in ggplot. It then creates the plots
 #'and saves them in a specified directory.
 #'
 #'
+#'this function requires the following functions:
+#'getYM, getYMall, smoothEach
+#'
 #' @export
-combineData <- function(n = 300, path = getwd(), condition = "condition"){
+combineData <- function(n = 50, path = getwd(), condition = "condition"){
 
   library(plyr)
   library(smoother)
   library(reshape2)
+  library(stringr)
 
   #choose ftirSEM file and mech tests
-  ftirSemPath <- file.choose()
+  ftirSemPath   <- file.choose()
   mechtestsPath <- choose.files()
+  ftykDataPath  <- choose.files()
 
   #read ftirsem
   ftirSem <- read.csv(ftirSemPath)
@@ -28,9 +33,32 @@ combineData <- function(n = 300, path = getwd(), condition = "condition"){
   renamed <- substr(renamed, 1, 2)
   names(mechtestsPath) <- renamed
 
+  # and fityK data
+  names(ftykDataPath) <- basename(ftykDataPath)
+  renamedFityk <- names(ftykDataPath)
+  renamedFityk <- substr(renamedFityk, 2, 3)
+  names(ftykDataPath) <- renamedFityk
+
+  #Read mechtests data
   allMech <- ldply(mechtestsPath, read.csv)
   allMech <- allMech[1:4]
   names(allMech) <- c('temp','time','forces','stroke')
+
+  #Read fityk data
+  fitykData <- ldply(ftykDataPath, read.delim, header=FALSE, comment.char="#")
+  fitykData <- fitykData[1:6]
+  names(fitykData) <- c('Temp','Peak','Center','Height','Area','FWHM')
+  fitykData$Peak <- substr(fitykData$Peak, 3, 6)
+
+  #make new dataframe of peak data that has percent crystallinity and orientation calculated
+  fitykCalcs <- getPeakAreaAll(fitykData)
+  fitykCalcs$Crystallinity <- fitykCalcs$area1725/(fitykCalcs$area1725 + fitykCalcs$area1736*1.46)
+
+  #need to get the 1157 peak fitted in fityk first so I can do the orientation calculation
+
+
+  #merge the fityk table with the imported data.frame
+  ftirSem2 <- merge(ftirSem, fitykCalcs)
 
   #Get rid of non numeric headers for each temperature
   allMech <- allMech[!is.na(as.numeric(as.character(allMech$force))),]
@@ -40,24 +68,24 @@ combineData <- function(n = 300, path = getwd(), condition = "condition"){
   allMech$stroke <- as.numeric(as.character(allMech$stroke))
   allMech$time <- as.numeric(as.character(allMech$time))
 
-  allMech <- smoothEach(allMech, .01, n = n)
+  allMech <- smoothEach(allMech, .01, n = 100)
   allMech <- na.omit(allMech)
 
   # Merge allMech and ftirSem and calculate stress vs strain
 
   all <- merge(allMech, ftirSem, by = 'temp')
   all$stressFTIR <- all$forces/all$CSA_FTIR/1000
-  all$stressSEM <- all$forces/all$CSA_SEM/1000
-  all$strain <- all$stroke/12.5
+  all$stressSEM  <- all$forces/all$CSA_SEM/1000
+  all$strain     <- all$stroke/12.5
 
 #get youngs modulus and maximum strength
-all <- getYMall(all)
-all$index <- seq(1, dim(all)[1],1)
+  all <- getYMall(all)
+  all$index <- seq(1, dim(all)[1],1)
 
-cond <- rep(condition, length(all$temp))
-all$condition <- cond
+  cond <- rep(condition, length(all$temp))
+  all$condition <- cond
 
-all <- na.omit(all)
+  all <- na.omit(all)
 
 return(all)
 }
---
+++ R/peakAreas.R
@@ -0,0 +1,41 @@
+#this function gets the peak area for one peak
+
+getPeakArea <- function(data.frame, peak){
+
+  indi <- unique(data.frame$Temp)
+
+  peakx <- filter(data.frame, Peak == peak)
+
+  y <- data.frame()
+  x <- data.frame()
+
+  for (i in 1:length(indi)){
+
+    x <- peakx[peakx$Temp == indi[i],]
+    sum <- sum(x$Area)
+    y <- rbind(y, sum)
+  }
+
+names(y) <- paste("area",peak, sep = "")
+return(y)
+}
+
+
+#this function gets the area for all the peaks, using the above function
+
+getPeakAreaAll <- function(data.frame){
+
+  indi <- unique(data.frame$Peak)
+
+  y <- data.frame(unique(data.frame$Temp))
+  names(y) <- "Temp"
+  x <- data.frame()
+
+  for (i in 1:length(indi)){
+    x <- data.frame[data.frame$Peak == indi[i],]
+    x <- getPeakArea(x, indi[i])
+    y <- cbind(y,x)
+  }
+#y <- y[1:length(unique(data.frame$Peak))]
+return(y)
+}
